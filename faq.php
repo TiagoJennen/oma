d25a991ddea75b09732ba0fac7e6d2bc7434d12e
TiagoJennen <113349775+TiagoJennen@users.noreply.github.com>
2023-05-31 14:13:45 +0200

    faq update

--- faq.php
+++ faq.php
@@ -10,10 +10,16 @@
       rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
       integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
       crossorigin="anonymous"
-      referrerpolicy="no-referrer"
+      referrerpolic2y="no-referrer"
     />
   </head>
   <body>
+  <div class="menu">
+    <img class="menuFoto" src="pro.png" style="margin-left: 230px;" alt="">
+    <img class="menuFoto" src="pro.png" alt="">
+    <img class="menuFoto" src="pro.png" alt="">
+    <img class="menuFoto" src="pro.png" alt="">
+  </div>
     <div class="wrapper">
       <h1>FAQ</h1>
       <p>Veel gestelde vragen</p>
