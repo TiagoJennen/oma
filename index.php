be79083d896f0f1f9f2df1a200bdd865fb459387
Abdi7309 <512875@vistacollege.nl>
2023-06-09 12:54:14 +0200

    home page en profiel buttons

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 <body>
   <div id="image">
   </div>
-  <div id="box1"><img class="menuFoto" src="img/info.png"  alt="" onclick="window.location.href='./info.php'"></div>
-  <div id="box2"><img class="menuFoto" src="img/gym.png" alt="" onclick="window.location.href='./gym.php'"></div>
-  <div id="box3"><img class="menuFoto" src="img/profiel.png" alt="" onclick="window.location.href='./profiel.php'"></div>
-  <div id="box4"><img class="menuFoto" src="img/faqpng.png" alt="" onclick="window.location.href='./faq.php'"></div>
+  <div id="box1" onclick="window.location.href='./info.php'"><img class="menuFoto" src="img/info.png"  alt=""></div>
+  <div id="box2" onclick="window.location.href='./gym.php'"><img class="menuFoto" src="img/gym.png" alt="" onclick="window.location.href='./gym.php'"></div>
+  <div id="box3" onclick="window.location.href='./profiel.php'"><img class="menuFoto" src="img/profiel.png" alt="" onclick="window.location.href='./profiel.php'"></div>
+  <div id="box4" onclick="window.location.href='./faq.php'"><img class="menuFoto" src="img/faqpng.png" alt="" onclick="window.location.href='./faq.php'"></div>
 </body>
 </html>
